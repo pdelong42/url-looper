7b74716e5fa8a20f11539f643279674b1c989249
Paul DeLong <pd72@nyu.edu>
2014-10-14 17:57:01 -0400

    oops, stray trailing whitespace *tsk-tsk*;

--- project.clj
+++ project.clj
@@ -11,5 +11,5 @@
       [org.clojure/clojure       "1.5.1"]
       [org.clojure/tools.cli     "0.3.1"]
       [org.clojure/tools.logging "0.2.6"]
-      [org.slf4j/slf4j-log4j12   "1.7.1"]  ] 
+      [org.slf4j/slf4j-log4j12   "1.7.1"]  ]
    :main url-looper.core  )
